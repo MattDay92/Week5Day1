968daabfb1dc7667b652e19ef22a0b92016a1742
Matt Day <mattday@matts-mac-mini.lan>
2023-01-20 06:59:39 -0500

    third commit

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,11 @@
 from app import app
 # from folder called app - import the variable app from __init__.py
 
-from flask import render_template
+from flask import render_template, request, redirect, url_for
+from .forms import UserCreationForm
+from .models import User
 
+@app.route('/index')
 @app.route('/')
 def homePage():
     people = ['Shoha', 'Brandt', 'Aubrey']
@@ -16,3 +19,29 @@ def homePage():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signUpPage():
+    form = UserCreationForm()
+    if request.method == 'POST':
+        if form.validate():
+            username = form.username.data
+            email = form.email.data
+            password = form.password.data
+
+            print(username, email, password)
+
+            # add user to database
+            user = User(username, email, password)
+            
+            user.saveToDB()
+
+            return redirect(url_for('contact'))
+            # provide the name of the FUNCTION we're redirecting to
+
+
+    return render_template('signup.html', form = form)
